188deb4626d79e27bb3bf265a0f9e99c7c0a816a
himalakshmi <124651043+himalakshmi@users.noreply.github.com>
2023-06-08 10:24:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(digits) {
+if (!digits) {
+return [];
 }
 
+const mappings = {
+'2': 'abc',
+'3': 'def',
+'4': 'ghi','5': 'jkl',
+'6': 'mno',
+'7': 'pqrs',
+'8': 'tuv',
+'9': 'wxyz'
+};
+
+const results = [];
+	function generateCombinations(currentIndex, currentString) {
+if (currentIndex === digits.length) {
+results.push(currentString);
+return;
+}
+
+const currentDigit = digits[currentIndex];
+const letters = mappings[currentDigit];
+for (let i = 0; i < letters.length; i++) {
+const letter = letters[i];
+generateCombinations(currentIndex + 1, currentString + letter);
+}
+}
+
+generateCombinations(0, '');
+return results.sort();
+}
 module.exports = letterCombinations;
